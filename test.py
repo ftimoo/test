2fcbb468ceac6c6ad6510da23689e8ff3bbe9d5c
timo <46718300+ftimoo@users.noreply.github.com>
2023-10-04 12:22:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ word2 = st.text_input('Movie title', 'Life of Brian')
 
 word3 = st.text_input('Movie title', 'Life of Brian')
 
+st.button("Reset", type="primary")
 if st.button('Say hello'):
     st.write('Why hello there')
 else:
