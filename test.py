a87bb3766c4f8de03a6d54f2761eadef1908fd0d
timo <46718300+ftimoo@users.noreply.github.com>
2023-10-04 12:28:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from simpleai.search import CspProblem, backtrack
 
 word1 = st.text_input('word1', '')
 
@@ -6,8 +7,48 @@ word2 = st.text_input('word2', '')
 
 word3 = st.text_input('word3', '')
 
-st.button("Reset", type="primary")
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+def constraint_unique(variables, values):
+    return len(values) == len(set(values))  # remove repeated values and count
+
+def createNum(values):
+    returnval = ""
+    for i in values:
+        returnval += str(values[i])
+    return int(returnval)
+
+def constraint_add(variables, values):
+    factor1 = int(''.join(str(values[variables.index(c)]) for c in word1))
+    factor2 = int(''.join(str(values[variables.index(c)]) for c in word2))
+    result = int(''.join(str(values[variables.index(c)]) for c in word3))
+    return (factor1 + factor2) == result
+
+if word1 and word2 and word3:
+    variables = []
+    domains = {}
+    for var in word1[0]+word2[0]+word3[0]:
+        if var not in variables:
+            variables.append(var.upper())
+            domains[var] = list(range(1, 10))
+    for var in word1+word2+word3:
+        if var not in variables:
+            variables.append(var.upper())
+            domains[var] = list(range(0, 10))
+    
+    assert len(variables)<=10  , "To many diffyrent letter"
+        
+        
+    # the list of values that each variable can take
+    
+    
+    
+    
+    constraints = [
+        (tuple(variables), constraint_unique),
+        (tuple(variables), constraint_add),
+    ]
+    
+    
+    problem = CspProblem(tuple(variables), domains, constraints)
+    
+    output = backtrack(problem)
+    st.write('\nSolutions:', output)
